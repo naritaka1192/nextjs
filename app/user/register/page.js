11b76b32af847ebcbce06337be60d4bc9683909a
naritaka1192 <151434061+naritaka1192@users.noreply.github.com>
2024-05-26 22:40:22 +0900

    5/26 22:40

--- app/user/register/page.js
+++ app/user/register/page.js
@@ -7,7 +7,7 @@ const Register =()=>{
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
 
-  const handleSubmit=async(e)=>{
+  const handleSubmit = async(e)=>{
     e.preventDefault()
     try{
       const response=await fetch("http://localhost:3000/api/user/register",{
@@ -16,7 +16,7 @@ const Register =()=>{
           "Accept":"application/json",
           "Content-Type":"application/json"
         },
-        body:Json.stringify({
+        body:JSON.stringify({
           name: name,
           email: email,
           password: password
