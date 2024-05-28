988b270d40c741c49c9d045e05af554b2bd79394
naritaka1192 <151434061+naritaka1192@users.noreply.github.com>
2024-05-28 20:34:06 +0900

    Add files via upload

---
+++ app/item/readsingle/[id]/page.js
@@ -0,0 +1,14 @@
+const getSingleItem=async(id)=>{
+  console.log(id)
+  const response = await fetch("http://localhost:3000/api/item/readsingle/${id}")
+  const jsonData= await response.json()
+  console.log(jsonData)
+}
+
+const ReadSingleItem = (context) =>{
+getSingleItem(context.params.id)
+  return(
+    <h1>個別アイテムページ</h1>
+  )
+}
+export default ReadSingleItem
