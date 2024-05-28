f12c058627d0a5215120ef25dae14d8adf3f2038
naritaka1192 <151434061+naritaka1192@users.noreply.github.com>
2024-05-28 21:07:01 +0900

    5/28 できた

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,14 +1,28 @@
+import Image from "next/image"
+
 const getSingleItem=async(id)=>{
   console.log(id)
-  const response = await fetch(`http://localhost:3000/api/item/readsingle/${id}`)
+  const response = await fetch(`http://localhost:3000/api/item/readsingle/${id}`,{cache:"no-store"})
   const jsonData= await response.json()
-  console.log(jsonData)
+  const singleItem = jsonData.singleItem
+  return singleItem
 }
 
-const ReadSingleItem = (context) =>{
-getSingleItem(context.params.id)
+const ReadSingleItem = async(context) =>{
+  const singleItem = await getSingleItem(context.params.id)
+
   return(
-    <h1>個別アイテムページ</h1>
+    <div>
+      <div>
+        <Image src={singleItem.image} width={750} height={500} alt="item-image" priority />
+      </div>
+      <div>
+        <h1>{singleItem.title}</h1>
+        <h2>￥{singleItem.price}</h2>
+        <hr/>
+        <p>{singleItem.description}</p>
+      </div>
+    </div>
   )
 }
 export default ReadSingleItem
