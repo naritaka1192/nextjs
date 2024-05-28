0ff682afc50d990ab9b99623662e88b886a9174d
naritaka1192 <151434061+naritaka1192@users.noreply.github.com>
2024-05-28 20:12:23 +0900

    5/28 変更

--- app/page.js
+++ app/page.js
@@ -1,10 +1,28 @@
-const getAllItems=()=>{}
+import Link from "next/link"
+import Image from "next/image"
 
-const ReadAllItems =()=>{
+const getAllItems= async () => {
+  const response = await fetch("http://localhost:3000/api/item/readall",{cache:"no-store"})
+  const jsonData = await response.json()
+  const allItems = jsonData.allItems
+  return allItems
+}
+
+const ReadAllItems =async ()=>{
+  const allItems = await getAllItems()
   return(
     <div>
-      <h1 className="hi-style">こんにちは</h1>
-      <h2>さようなら</h2>
+      {allItems.map(item=>
+      <Link href="" key={item._id}>
+        <Image src={item.image} width={750} height={500} alt="item-image" priority/>
+
+        <div key={item.id}>
+          <h2>￥{item.price}</h2>
+          <h3>{item.title}</h3>
+          <p>{item.description.substring(0,80)}...</p>
+        </div>
+        </Link>
+      )}
     </div>
   )
 }
