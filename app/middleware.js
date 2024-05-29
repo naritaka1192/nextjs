3926d7d997e0337f327846ced69044f289cbf82f
naritaka1192 <151434061+naritaka1192@users.noreply.github.com>
2024-05-29 22:09:14 +0900

    5/29　家にて

--- app/middleware.js
+++ app/middleware.js
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 export async function middleware(request){
-  const token ="eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImR1bW15QGdtYWlsLmNvbSIsImV4cCI6MTcxNjU1MzA0MX0.eaF1n7MB0RSZPp4VXVnBYZyFD-xb_UHY0j_F75K650Y"
-
-  //await request.headers.get("Authorization")?.split(" ")[1]
+  const token = await request.headers.get("Authorization")?.split(" ")[1]
 
   if(!token){
     return NextResponse.json({message:"トークンがありません"})
